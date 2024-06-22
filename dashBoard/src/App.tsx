2f32b77d7a38a4016db8b748c56c6e47add8a005
sunnykr11 <sunny@zillout.com>
2024-06-22 14:02:58 +0530

    new changes

--- dashBoard/src/App.tsx
+++ dashBoard/src/App.tsx
@@ -1,9 +1,23 @@
 import './App.css';
 import { Dashboard } from './pages/Dashboard';
 import Chart from './pages/Chart';
+import { ChangeEvent, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+
+
 function App() {
+  const [inputText, setInputText] = useState("");
+    const factor = (e: ChangeEvent<HTMLInputElement>) => {
+      setInputText(e.target.value)
+
+
+      const factorial = e.target.value
+      for(let i = factorial-1 )
+
+    console.log(factorial);
+    
+  }
   return (
     <div className="App">
       <Router>
@@ -12,6 +26,11 @@ function App() {
           <Route path="/pages/chart" element={<Chart />} />
         </Routes>
       </Router>
+      <div className='input-holder'>
+      <input type="number" onChange={factor} value={inputText} className='input-field' />
+      <p>Input: {inputText}</p>
+      <button className='btn'>Submit</button>
+      </div>
     </div>
   );
 }
